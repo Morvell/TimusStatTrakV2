f65f0681f1341dda3875ad91a3aef43422ed6bcb
morvell <zaxarov2702@yandex.ru>
2017-03-07 00:31:34 +0500

    Добавлена ссылка на автора

--- app/src/main/java/xyz/jormungand/timusstattrak/AboutActivity.java
+++ app/src/main/java/xyz/jormungand/timusstattrak/AboutActivity.java
@@ -5,6 +5,8 @@ import com.google.android.gms.ads.AdView;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -13,11 +15,16 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        TextView textView = (TextView) findViewById(R.id.textViewAZSoftware);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
         // Load an ad into the AdMob banner view.
         AdView adView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder()
                 .setRequestAgent("ca-app-pub-8467305463887516~6261728988").build();
         adView.loadAd(adRequest);
+
+
 
     }
 
